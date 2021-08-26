13d20613bb6f82422e411fadbda9943198b60c63
hammad <hamcan2550@gmail.com>
2021-08-26 15:50:44 +0100

    commit

--- src/app/purchase-order-line-item/purchase-order-line-item.component.ts
+++ src/app/purchase-order-line-item/purchase-order-line-item.component.ts
@@ -12,4 +12,12 @@ export class PurchaseOrderLineItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  elements: any = [
+    {id: 1, first: 'Mark', last: 'Otto', handle: '@mdo'},
+    {id: 2, first: 'Jacob', last: 'Thornton', handle: '@fat'},
+    {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
+  ];
+
+  headElements = ['ID', 'First', 'Last', 'Handle'];
+
 }
