9228356898844a3391de0aea377d2b81fba6fa60
hammad <hamcan2550@gmail.com>
2021-08-28 15:57:01 +0100

    Form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ksb-scip-app';
-  isLoggenIn:boolean = false;
-  constructor(private router: Router) { }
+  isLoggenIn: boolean = false;
 
-  authenticate(){
-    this.isLoggenIn=true;
+  constructor(private router: Router) {}
+
+  authenticate(f: NgForm) {
+    this.isLoggenIn = true;
+    if (f.value.tokenInput != '') {
+      console.log('Input Token: ' + f.value.tokenInput);
+    } else {
+      alert('Fill the name first!!!');
+    }
     this.router.navigateByUrl('/purchase-order-line-item');
   }
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PurchaseOrderLineItemComponent } from './purchase-order-line-item/purchase-order-line-item.component';
-import { GeneratetokenComponent } from './generatetoken/generatetoken.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PurchaseOrderLineItemComponent,
-    GeneratetokenComponent
+    PurchaseOrderLineItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
