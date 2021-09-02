8d3f1fe1353ea1a392e0acc25efdaa0b1467a68b
hammad <hamcan2550@gmail.com>
2021-09-02 11:17:30 +0100

    table

--- src/app/purchase-order-line-item/purchase-order-line-item.component.ts
+++ src/app/purchase-order-line-item/purchase-order-line-item.component.ts
@@ -1,76 +1,127 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '.././app.component';
-import { faPlus,faGreaterThan,faLessThan,faFilePdf } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlus,
+  faGreaterThan,
+  faLessThan,
+  faFilePdf,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-purchase-order-line-item',
   templateUrl: './purchase-order-line-item.component.html',
-  styleUrls: ['./purchase-order-line-item.component.css']
+  styleUrls: ['./purchase-order-line-item.component.css'],
 })
 export class PurchaseOrderLineItemComponent implements OnInit {
   faPlus = faPlus;
   faLessThan = faLessThan;
   faGreaterThan = faGreaterThan;
   faFilePdf = faFilePdf;
-  iconPlusCircle = 'assets/Image/pluscircle.png' 
-  iconPlusAlt="add"
+  iconPlusCircle = 'assets/Image/pluscircle.png';
+  iconPlusAlt = 'add';
+  iconPDF = 'assets/Image/pdficon2.png';
+  iconPDFAlt = 'pdf';
+  iconArrow = 'assets/Image/arrowicon.png';
+  iconArrowAlt = 'select';
   constructor(public appComponent: AppComponent) {
     appComponent.isLoggenIn = true;
-   }
+  }
+  isCatergoryList: Boolean = true;
+  selectedCat: string = '';
+  // showMaterialCatList() {
+  //   this.isCatergoryList = !this.isCatergoryList;
+  // }
+  selectMaterial(item, parentIndex, elements) {
 
-  ngOnInit(): void {
+    this.isCatergoryList = true;
+    this.selectedCat = item;
+    this.isCatergoryList = !this.isCatergoryList;
+
+    if (item !='' && item != 'materialCategory') {
+      elements[parentIndex].isSelected = true;
+      elements[parentIndex].selectedCat = item;
+      // for (var i = 0; i < materialCatList.length; i++) {
+      //   if (i == index) {
+      //     this.isCatergoryList = true;
+      //     this.selectedCat = item;
+      //   }
+      // }     
+    }
   }
 
-  elements: any = [ {
-    "statisticalGoodsNumber" : 5,
-    "CASNumber" : 5,
-    "purchaseOrderNumber" : 45654756,
-    "scipNumber" : 1,
-    "lineItemNumber" : 6,
-    "scipRelevent" : "Yes",
-    "materialCategory" : "materialCategory",
-    "submitStatus" : "submitted"
-  }, {
-    "statisticalGoodsNumber" : 5,
-    "CASNumber" : 5,
-    "purchaseOrderNumber" : 45654756,
-    "scipNumber" : 1,
-    "lineItemNumber" : 6,
-    "scipRelevent" : "Yes",
-    "materialCategory" : "materialCategory",
-    "submitStatus" : "submitted"
-  },
-  {
-    "statisticalGoodsNumber" : 5,
-    "CASNumber" : 5,
-    "purchaseOrderNumber" : 45654756,
-    "scipNumber" : 1,
-    "lineItemNumber" : 6,
-    "scipRelevent" : "Yes",
-    "materialCategory" : "materialCategory",
-    "submitStatus" : "submitted"
-  },
-  {
-    "statisticalGoodsNumber" : 9,
-    "CASNumber" : 5,
-    "purchaseOrderNumber" : 45654756,
-    "scipNumber" : 1,
-    "lineItemNumber" : 6,
-    "scipRelevent" : "Yes",
-    "materialCategory" : "materialCategory",
-    "submitStatus" : "submitted"
-  },
-  {
-    "statisticalGoodsNumber" : 7,
-    "CASNumber" : 5,
-    "purchaseOrderNumber" : 45654756,
-    "scipNumber" : 1,
-    "lineItemNumber" : 6,
-    "scipRelevent" : "Yes",
-    "materialCategory" : "materialCategory",
-    "submitStatus" : "submitted"
-  } ];
+  ngOnInit(): void {}
 
-  headElements = ['Purchase Order', 'Line Item', 'SCIP Relevant', 'SCIP No.', 'Statistical Goods No', 'CAS No', 'Material Category','Action'];
+  elements: any = [
+    {
+      statisticalGoodsNumber: 5,
+      CASNumber: 5,
+      purchaseOrderNumber: 45654756,
+      scipNumber: 1,
+      lineItemNumber: 6,
+      scipRelevent: 'Yes',
+      materialCategory: ['sample56', 'sample2', 'sample3'],
+      submitStatus: 'submitted',
+      isSelected: false,
+      selectedCat: ''
+    },
+    {
+      statisticalGoodsNumber: 5,
+      CASNumber: 5,
+      purchaseOrderNumber: 45654756,
+      scipNumber: 1,
+      lineItemNumber: 6,
+      scipRelevent: 'Yes',
+      materialCategory: ['sample1', 'sample2', 'sample89'],
+      submitStatus: 'submitted',
+      isSelected: false,
+      selectedCat: ''
+    },
+    {
+      statisticalGoodsNumber: 5,
+      CASNumber: 5,
+      purchaseOrderNumber: 45654756,
+      scipNumber: 1,
+      lineItemNumber: 6,
+      scipRelevent: 'Yes',
+      materialCategory: ['sample1', 'sample2', 'sample3'],
+      submitStatus: 'submitted',
+      isSelected: false,
+      selectedCat: ''
+    },
+    {
+      statisticalGoodsNumber: 9,
+      CASNumber: 5,
+      purchaseOrderNumber: 45654756,
+      scipNumber: 1,
+      lineItemNumber: 6,
+      scipRelevent: 'Yes',
+      materialCategory: ['sample1', 'sample2', 'sample3'],
+      submitStatus: 'submitted',
+      isSelected: false,
+      selectedCat: ''
+    },
+    {
+      statisticalGoodsNumber: 7,
+      CASNumber: 5,
+      purchaseOrderNumber: 45654756,
+      scipNumber: 1,
+      lineItemNumber: 6,
+      scipRelevent: 'Yes',
+      materialCategory: ['sample5', 'sample2', 'sample3'],
+      submitStatus: 'submitted',
+      isSelected: false,
+      selectedCat: ''
+    },
+  ];
 
+  headElements = [
+    'Purchase Order',
+    'Line Item',
+    'SCIP Relevant',
+    'SCIP No.',
+    'Statistical Goods No',
+    'CAS No',
+    'Material Category',
+    'Action',
+  ];
 }
