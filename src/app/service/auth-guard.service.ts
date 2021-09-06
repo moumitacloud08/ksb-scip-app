6198beb31bef19fd8c2067d0732b3933208d1bb2
hammad <hamcan2550@gmail.com>
2021-09-06 19:17:29 +0100

    authguard redirect

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { LocalStorageService } from 'ngx-webstorage';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private localStorageService: LocalStorageService) { }
+  constructor(private localStorageService: LocalStorageService, private router: Router) { }
 
   canActivate(): boolean {
     console.log("Inside AuthGuardService")
@@ -18,6 +19,7 @@ export class AuthGuardService implements CanActivate {
       console.log("Inside AuthGuardService TRUE")
       return true;
     }
+    this.router.navigateByUrl('/');
     return false;
   }
 }
