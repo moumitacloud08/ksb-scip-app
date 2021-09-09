8f4ab9482550b7505351ee26237607de9ab5c020
hammad <hamcan2550@gmail.com>
2021-09-09 06:22:19 +0100

    Base Auth dded in login API

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Message } from '../message.model';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry,tap } from 'rxjs/operators';
+import { catchError, retry, tap } from 'rxjs/operators';
 import * as cons from '../constants';
+import { LocalStorageService } from 'ngx-webstorage';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -16,7 +17,8 @@ const httpOptions = {
 })
 export class LoginService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private localStorageService: LocalStorageService
   ) {}
 
   handleError(error) {
@@ -28,7 +30,7 @@ export class LoginService {
       // server-side error
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
-    window.alert(errorMessage);
+    // window.alert(errorMessage);
     return throwError(errorMessage);
   }
   private tokenUrl =
@@ -41,12 +43,15 @@ export class LoginService {
       console.log('token not blank ');
       httpOptions.headers = new HttpHeaders({
         api_token: token,
+        'Content-Type': 'application/json',
+        Authorization:
+          'Basic ' + this.localStorageService.retrieve('user').authToken,
       });
     }
     console.log('tokenUrl ' + this.tokenUrl);
-    return this.http
-      .get<any[]>(this.tokenUrl, httpOptions)
-      .pipe(tap(data =>
-        console.log('All: ' + JSON.stringify(data))), catchError(this.handleError));
+    return this.http.get<any[]>(this.tokenUrl, httpOptions).pipe(
+      tap((data) => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   }
 }
