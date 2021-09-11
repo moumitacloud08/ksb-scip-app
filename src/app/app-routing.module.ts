5025b9ced3081b8d66d1797656566eb942fb7fd9
hammad <hamcan2550@gmail.com>
2021-09-11 09:06:45 +0100

    design modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,6 +30,7 @@ const routes: Routes = [
             {
                 path: 'purchase-order-line-item',
                 component: PurchaseOrderLineItemComponent
+                // component: PurchaseOrderLineItemComponent,canActivate:[AuthGuard]
             }
         ]
     }
