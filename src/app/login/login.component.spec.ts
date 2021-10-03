72ab208e29d5e47dd75e500255c9206e086b964b
hammad <hamcan2550@gmail.com>
2021-10-03 12:53:06 +0530

    junit

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,3 +1,95 @@
+import {
+  inject,
+  tick,
+  TestBed,
+  getTestBed,
+  async,
+  fakeAsync,
+  ComponentFixture,
+} from '@angular/core/testing';
+import { LoginComponent } from './login.component';
+import {
+  NgxWebstorageModule,
+  LocalStorageService,
+} from 'ngx-webstorage';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+import { LoginService } from '../service/login.service';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { RouterTestingModule } from "@angular/router/testing";
+import { TranslateTestingModule } from 'ngx-translate-testing';
+import { TranslateService, TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let service: LoginService;
+  let localStorageService: LocalStorageService;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        NgxWebstorageModule.forRoot(),
+        RouterTestingModule,
+        TranslateTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        }),
+        FormsModule
+      ],
+      declarations: [LoginComponent],
+      providers: [LocalStorageService, TranslateService,]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    service = TestBed.inject(LoginService);
+    localStorageService = TestBed.inject(LocalStorageService);
+    spyOn(localStorageService, 'clear').and.returnValue();
+    //spyOn(localStorageService, 'store').and.returnValue("6765");
+    let store = {"app":"scip","key":"6765","lang":"en"}
+    spyOn(localStorageService, 'retrieve').and.callFake((key) =>{return store[key]});
+    fixture.detectChanges();
+  });
+
+  it('Test authenticate EqualTo', fakeAsync(() => {
+    const testForm = <NgForm>{
+      value: {
+        tokenInput: '123456789',
+      },
+    };
+
+    let message = [{ code: '200', type: 'String', message: 'Login Success' }];
+    spyOn(service, 'login').and.returnValue(of([message]));
+    component.authenticate(testForm);
+    expect(component.messages.length).toBe(1);
+  }));
+  
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+
+
+
+
+
+
+
 // import {
 //   inject,
 //   tick,
