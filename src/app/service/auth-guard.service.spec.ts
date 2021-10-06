285aeeea3eba19e687e16ed7e91b2825d4e55674
hammad <hamcan2550@gmail.com>
2021-10-06 18:36:20 +0530

    unit test

--- src/app/service/auth-guard.service.spec.ts
+++ src/app/service/auth-guard.service.spec.ts
@@ -1,16 +1,41 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { AuthGuardService } from './auth-guard.service';
+import { AuthGuardService } from './auth-guard.service';
+import {
+    NgxWebstorageModule,
+    LocalStorageService,
+} from 'ngx-webstorage';
+import { RouterTestingModule } from "@angular/router/testing";
 
-// describe('AuthGuardService', () => {
-//   let service: AuthGuardService;
+describe('AuthGuardService', () => {
+    let service: AuthGuardService;
+    let localStorageService: LocalStorageService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(AuthGuardService);
-//   });
-
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                NgxWebstorageModule.forRoot(),
+                RouterTestingModule,
+            ],
+            providers: [LocalStorageService]
+        });
+        localStorageService = TestBed.inject(LocalStorageService);
+       
+        service = TestBed.inject(AuthGuardService);
+    });
+    it('Test canActive tobe true', () => {
+        let store = { "app": "scip", "key": "6765", "lang": "en", "user": { authToken: 'a3NiOmtzYg==' ,valid:true}, "api_token": "123456789" }
+        spyOn(localStorageService, 'retrieve').and.callFake((key) => { return store[key] });
+        let isActive = service.canActivate()
+        expect(isActive).toBe(true);
+      });
+      it('Test canActive tobe false', () => {
+        let store = { "app": "scip", "key": "6765", "lang": "en", "user": { authToken: 'a3NiOmtzYg==' ,valid:false}, "api_token": "123456789" }
+        spyOn(localStorageService, 'retrieve').and.callFake((key) => { return store[key] });
+        let isActive = service.canActivate()
+        expect(isActive).toBe(false);
+      });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+});
