3e9b089be246919745f92264387a63bfba685c3c
hammad <hamcan2550@gmail.com>
2021-10-13 10:53:40 +0530

    lang added

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 'use strict';
 export const  BASE_URL ='http://localhost:9002/scip-api'; 
-export const langArray = ['en', 'nl']
-export const langList = [{code:'en',desc:'English'},{code:'nl',desc:'Dutch'}]
+export const langArray = ['en', 'de','fr','pt']
+export const langList = [{code:'en',desc:'English'},{code:'de',desc:'German'},{code:'fr',desc:'French'},{code:'pt',desc:'Potugese'}]
 export const  DEFAULT_LANG ='en'; 
